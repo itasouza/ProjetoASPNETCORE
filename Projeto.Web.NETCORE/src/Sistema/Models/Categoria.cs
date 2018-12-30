0e4bd9ff5737fdd118aed593e1f9e19956bf0cdc
itasouza <itasouza@yahoo.com.br>
2018-12-30 17:19:36 -0200

    Criação da classe de categoria e validação usando DataAnnotations

---
+++ Projeto.Web.NETCORE/src/Sistema/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        [Required(ErrorMessage ="Campo Requerido")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="O nome deve ter entre 3 a 50 caracteres")]
+        public string Nome { get; set; }
+
+        [StringLength(256,ErrorMessage ="A descrição não deve exceder 256 caracteres")]
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
