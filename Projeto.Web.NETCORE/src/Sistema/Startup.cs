489b6e358531533defa334a929ac60c522a4f40d
itasouza <itasouza@yahoo.com.br>
2018-12-30 17:32:11 -0200

    Criação do controle de categoria e geração das telas + conexão ao banco de dados

--- Projeto.Web.NETCORE/src/Sistema/Startup.cs
+++ Projeto.Web.NETCORE/src/Sistema/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Models;
 
 namespace Sistema
 {
@@ -32,6 +34,9 @@ namespace Sistema
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SistemaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SistemaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
